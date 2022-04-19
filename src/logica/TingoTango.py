07b465bff90a29e59fc8a8a6010b11ec84bdfa15
Samantha2307 <72857554@continental.edu.pe>
2022-04-18 23:48:09 -0500

    Múltiplo de 5 implementado V2.0

--- src/logica/TingoTango.py
+++ src/logica/TingoTango.py
@@ -2,3 +2,6 @@ class TingoTango:
     def textoTingoTango(selfself,numero):
         if (numero % 3 == 0):
             return "Tingo"
+
+        if (numero % 5 == 0):
+            return "Tango"
--- test/TingoTangoTest.py
+++ test/TingoTangoTest.py
@@ -18,3 +18,14 @@ class TingoTangoPrueba(unittest.TestCase):
 
         # Assert
             self.assertEqual(self.resultadoEsperado,self.resultadoActual)
+
+    def test_TingoTango_multiploCinco_retornaTango(self):
+        # Arrange
+            self.numero = 5
+            self.resultadoEsperado = "Tango"
+
+        # Do
+            self.resultadoActual = self.TT.textoTingoTango(self.numero)
+
+        # Assert
+            self.assertEqual(self.resultadoEsperado,self.resultadoActual)
